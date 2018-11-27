24c2835b18d93b2fbc957fbabd664036eb91471a
MaksimTsedov <maksim.czedov.99@gmail.com>
2018-11-27 12:05:08 +0200

    Logging handed down in a separate dll

---
+++ Logging/LoggingToFile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EHospital.Logging
+{
+    public static class LoggingToFile
+    {
+        private static readonly log4net.ILog log = log4net.LogManager
+                                                          .GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public static void LoggingInfo(string message)
+        {
+            log.Info(message);
+        }
+
+        public static void LoggingWarn(string message)
+        {
+            log.Warn(message);
+        }
+
+        public static void LoggingWarn(string message, Exception ex)
+        {
+            log.Warn(message, ex);
+        }
+
+        public static void LoggingError(string message)
+        {
+            log.Error(message);
+        }
+
+        public static void LoggingError(string message, Exception ex)
+        {
+            log.Error(message, ex);
+        }
+    }
+}
